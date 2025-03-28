01577b3332a19ac53373581437f4eb99d2824e9f
Suzy-Lana777 <reutasvetlana382@gmail.com>
2025-03-28 15:29:25 +0200

    clear comment

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,35 +1,5 @@
 // написання функції для HTTP-запитів
 
-// import axios from 'axios';
-
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
-
-// const API_KEY = '49399736-5639b789053676a95c3f2e38d';  
-
-// let currentPage = 1;
-// export default async function httpRequest(userRequest, page = 1, perPage = 15) {
-//   const paramsForHttp = new URLSearchParams({
-//     q: userRequest,
-//     key: API_KEY,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-//     per_page: perPage,
-//     page: page,
-//   });
-
-//   try {
-//     const response = await axios.get(`?${paramsForHttp}`);
-//     return response.data;
-//   } catch (error) {
-//     throw new Error(`Failed to fetch images: ${error.message}`);
-//   }
-// }
-
-// // Скидаємо значення сторінки до 1
-// export function resetPage() {
-//   currentPage = 1;
-// }
 
 // ============
 
