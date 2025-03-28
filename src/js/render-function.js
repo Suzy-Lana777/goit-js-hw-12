9548ba76ea3c4097d87ed75b7c27ed122760274f
Suzy-Lana777 <reutasvetlana382@gmail.com>
2025-03-28 09:14:11 +0200

    last part

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -1,48 +1,48 @@
 // функції для відображення елементів інтерфейсу
 
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// export function createCart(item) {
-//   const {
-//     webformatURL,
-//     largeImageURL,
-//     tags,
-//     likes,
-//     views,
-//     comments,
-//     downloads,
-//   } = item;
+export function createCart(item) {
+  const {
+    webformatURL,
+    largeImageURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+  } = item;
 
-//   return `
-//     <li class="li-gallery">
-//       <a class="li-gallery-a" href="${largeImageURL}">
-//         <img class="img" src="${webformatURL}" alt="${tags}" />
-//       </a>
-//       <ul class="baner-info">
-//         <li class="baner-info-li">
-//           <p class="baner-info-li-title">Likes</p>
-//           <p class="baner-info-li-text">${likes}</p>
-//         </li>
-//         <li class="baner-info-li">
-//           <p class="baner-info-li-title">Views</p>
-//           <p class="baner-info-li-text">${views}</p>
-//         </li>
-//         <li class="baner-info-li">
-//           <p class="baner-info-li-title">Comments</p>
-//           <p class="baner-info-li-text">${comments}</p>
-//         </li>
-//         <li class="baner-info-li">
-//           <p class="baner-info-li-title">Downloads</p>
-//           <p class="baner-info-li-text">${downloads}</p>
-//         </li>
-//       </ul>
-//     </li>`;
-// }
+  return `
+    <li class="li-gallery">
+      <a class="li-gallery-a" href="${largeImageURL}">
+        <img class="img" src="${webformatURL}" alt="${tags}" />
+      </a>
+      <ul class="baner-info">
+        <li class="baner-info-li">
+          <p class="baner-info-li-title">Likes</p>
+          <p class="baner-info-li-text">${likes}</p>
+        </li>
+        <li class="baner-info-li">
+          <p class="baner-info-li-title">Views</p>
+          <p class="baner-info-li-text">${views}</p>
+        </li>
+        <li class="baner-info-li">
+          <p class="baner-info-li-title">Comments</p>
+          <p class="baner-info-li-text">${comments}</p>
+        </li>
+        <li class="baner-info-li">
+          <p class="baner-info-li-title">Downloads</p>
+          <p class="baner-info-li-text">${downloads}</p>
+        </li>
+      </ul>
+    </li>`;
+}
 
-// export function createsCart(items) {
-//   return items.map(createCart).join('');
-// }
+export function createsCart(items) {
+  return items.map(createCart).join('');
+}
 
 export function clearGallery() {
   const gallery = document.querySelector('.gallery');
--- src/main.js
+++ src/main.js
@@ -1,63 +1,62 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import httpRequest from './js/pixabay-api';
+import { renderGallery, clearGallery } from './js/render-functions';
 
-// import iziToast from 'izitoast';
-// import 'izitoast/dist/css/iziToast.min.css';
-// import httpRequest from './js/pixabay-api';
-// import { renderGallery, clearGallery } from './js/render-functions';
+const refs = {
+  form: document.querySelector('.form'),
+  input: document.querySelector('.input'),
+  gallery: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader'),
+};
 
-// const refs = {
-//   form: document.querySelector('.form'),
-//   input: document.querySelector('.input'),
-//   gallery: document.querySelector('.gallery'),
-//   loader: document.querySelector('.loader'),
-// };
+refs.form.addEventListener('submit', request);
 
-// refs.form.addEventListener('submit', request);
+function request(event) {
+  event.preventDefault();
+  const userRequest = event.currentTarget.elements.user_request.value.trim();
 
-// function request(event) {
-//   event.preventDefault();
-//   const userRequest = event.currentTarget.elements.user_request.value.trim();
+  if (!userRequest) {
+    iziToast.warning({
+      title: 'WARNING',
+      message: 'Please enter a search term.',
+      position: 'topRight',
+    });
+    return;
+  }
 
-//   if (!userRequest) {
-//     iziToast.warning({
-//       title: 'WARNING',
-//       message: 'Please enter a search term.',
-//       position: 'topRight',
-//     });
-//     return;
-//   }
+  clearGallery();
+  showLoader();
 
-//   clearGallery();
-//   showLoader();
+  httpRequest(userRequest)
+    .then(images => {
+      hideLoader();
+      if (images.length === 0) {
+        iziToast.error({
+          title: 'ERROR',
+          message: 'No images found. Try another search term!',
+          position: 'topRight',
+        });
+        return;
+      }
+      renderGallery(images);
+    })
+    .catch(error => {
+      hideLoader();
+      iziToast.error({
+        title: 'ERROR',
+        message: `Error fetching images: ${error.message}`,
+        position: 'topRight',
+      });
+    });
 
-//   httpRequest(userRequest)
-//     .then(images => {
-//       hideLoader();
-//       if (images.length === 0) {
-//         iziToast.error({
-//           title: 'ERROR',
-//           message: 'No images found. Try another search term!',
-//           position: 'topRight',
-//         });
-//         return;
-//       }
-//       renderGallery(images);
-//     })
-//     .catch(error => {
-//       hideLoader();
-//       iziToast.error({
-//         title: 'ERROR',
-//         message: `Error fetching images: ${error.message}`,
-//         position: 'topRight',
-//       });
-//     });
+  event.target.reset();
+}
 
-//   event.target.reset();
-// }
+function showLoader() {
+  refs.loader.classList.remove('visually-hidden');  // Показуємо loader
+}
 
-// function showLoader() {
-//   refs.loader.classList.remove('visually-hidden');  // Показуємо loader
-// }
-
-// function hideLoader() {
-//   refs.loader.classList.add('visually-hidden');  // Ховаємо loader
-// }
+function hideLoader() {
+  refs.loader.classList.add('visually-hidden');  // Ховаємо loader
+}
