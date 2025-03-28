b92468e8cea2c8d4655f1bc53f7e33da98023350
Suzy-Lana777 <reutasvetlana382@gmail.com>
2025-03-28 08:48:19 +0200

    problem

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,23 +1,23 @@
 // // написання функції для HTTP-запитів
 
-import axios from 'axios';
+// import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';  // Це має бути правильно
+// axios.defaults.baseURL = 'https://pixabay.com/api/';  // Це має бути правильно
 
-const API_KEY = '49399736-5639b789053676a95c3f2e38d';  // Перевірити, чи ключ правильний
+// const API_KEY = '49399736-5639b789053676a95c3f2e38d';  // Перевірити, чи ключ правильний
 
-export default function httpRequest(userRequest) {
-  const paramsForHttp = new URLSearchParams({
-    q: userRequest, 
-    key: API_KEY,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
+// export default function httpRequest(userRequest) {
+//   const paramsForHttp = new URLSearchParams({
+//     q: userRequest, 
+//     key: API_KEY,
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//   });
 
-  return axios.get(`?${paramsForHttp}`)
-    .then(response => response.data.hits)  // Перевіряємо, чи відповідає структура даних
-    .catch(error => {
-      throw error;  // Повертати повну помилку, щоб вона була доступною в `index.js`
-    });
-}
+//   return axios.get(`?${paramsForHttp}`)
+//     .then(response => response.data.hits)  // Перевіряємо, чи відповідає структура даних
+//     .catch(error => {
+//       throw error;  // Повертати повну помилку, щоб вона була доступною в `index.js`
+//     });
+// }
